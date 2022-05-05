ba56d8a7d01c581b0ff61712cb9c2e2998816270
mariavitoria2022 <marparkminiekookiesilva@gmail.com>
2022-05-05 20:57:30 -0300

    exercicio lab: experimentação da página de cadastro de pessoas; exercício: incluir o atributo data de nascimento no sistema exemplo (02/05/2022 - 02/05/2022)

---
+++ exercicios/monopy/post/front-end/js/meuscript.js
@@ -0,0 +1,42 @@
+$(function() { // quando o documento estiver pronto/carregado
+    
+    // código para mapear click do botão incluir pessoa
+    $(document).on("click", "#btIncluirPessoa", function() {
+        //pegar dados da tela
+        nome = $("#campoNome").val();
+        nascimento = $("#campoData").val();
+        email = $("#campoEmail").val();
+        tel = $("#campoTelefone").val();
+        // preparar dados no formato json
+        var dados = JSON.stringify({ nome: nome, nascimento: nascimento, email: email, telefone: tel });
+        // fazer requisição para o back-end
+        $.ajax({
+            url: 'http://localhost:5000/incluir_pessoa',
+            type: 'POST',
+            dataType: 'json', // os dados são recebidos no formato json
+            contentType: 'application/json', // tipo dos dados enviados
+            data: dados, // estes são os dados enviados
+            success: pessoaIncluida, // chama a função listar para processar o resultado
+            error: erroAoIncluir
+        });
+        function pessoaIncluida (retorno) {
+            if (retorno.resultado == "ok") { // a operação deu certo?
+                // informar resultado de sucesso
+                alert("Pessoa incluída com sucesso!");
+                //$("#mensagem").text("Pessoa incluída com sucesso!");
+                // limpar os campos
+                $("#campoNome").val("");
+                $("#campoData").val("");
+                $("#campoEmail").val("");
+                $("#campoTelefone").val("");
+            } else {
+                // informar mensagem de erro
+                alert("ERRO na inclusão: "+retorno.resultado + ":" + retorno.detalhes);
+            }            
+        }
+        function erroAoIncluir (retorno) {
+            // informar mensagem de erro
+            alert("ERRO ao contactar back-end: "+retorno.resultado + ":" + retorno.detalhes);
+        }
+    });
+});
